5853089254a50e904b36770385ce6997a3744755
rabbitisa <rabbitisa@gmail.com>
2016-06-04 11:34:19 +0100

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,13 +1,14 @@
-getwd('C:\\Users\\bunny\\Documents\\data analysis\\Project1')
+#Project 1
 household_power_consumption <- read.csv("~/data analysis/Project1/household_power_consumption.txt", sep=";")
 #change date format
 household_power_consumption$Date <-as.Date(household_power_consumption$Date, format = "%d/%m/%Y")
 #subset the data
-data<-subset(household_power_consumption,subset=(Date>="2007-02-01"&Date<="2007-02-02"))
-GlobalActivePower <- as.numeric(data$Global_active_power)
+Data<-subset(household_power_consumption,subset=(Date>="2007-02-01"&Date<="2007-02-02"))
+GlobalActivePower<-as.numeric(as.character(Data$Global_active_power))
 
 #Plot 1
-png("plot1.png", width=480, height=480)
+png("Plot1.png", width=480, height=480)
 hist(GlobalActivePower, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
+
 
